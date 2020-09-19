0aa60efc0e50f5e3f7664019063f3e825a6850f9
mamiline6 <ohmamirineoo6@gmail.com>
2020-09-19 13:37:35 +0900

    2 まで進行

---
+++ 2 Attribute Binding/main.js
@@ -0,0 +1,9 @@
+var app = new Vue ({
+  el: '#app',
+  data: {
+    product: 'Socks',
+    linkUrl: 'https://www.google.com',
+    image: 'vmSocks-green-onWhite.jpg',
+    altText: 'green socks'
+  }
+})
