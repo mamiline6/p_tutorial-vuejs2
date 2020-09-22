76293465c4818083c9fcb7a548c69e72ff0c9b98
mamiline6 <ohmamirineoo6@gmail.com>
2020-09-22 15:03:42 +0900

    余分なファイル削除

--- 2 Attribute Binding/main.js
+++
@@ -1,9 +0,0 @@
-var app = new Vue ({
-  el: '#app',
-  data: {
-    product: 'Socks',
-    linkUrl: 'https://www.google.com',
-    image: 'vmSocks-green-onWhite.jpg',
-    altText: 'green socks'
-  }
-})
